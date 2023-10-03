ed5dedcf543b351dc09d2a7e790c7198540508cf
Rafael Viegas <rafael.viegas@arpagus.com.br>
2023-10-02 22:49:49 -0300

    Aula 08

---
+++ aula 08/components/counter.js
@@ -0,0 +1,10 @@
+const counter = function(id){
+
+    const input = document.createElement("input");
+    input.setAttribute("type", "number");
+    input.setAttribute("id", id);
+    input.setAttribute("value", "0");
+    input.classList.add("input-valid");
+    
+    return input;
+}
